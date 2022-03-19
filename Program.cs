f396c4d690e60f0dc12e6428b102b6daca3b74ba
Programador Michel Rosales <95501615+Michprosal003@users.noreply.github.com>
2022-03-19 18:20:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" ingrese la cantidad de Celsius :");
+
+            Console.Title="Conversion de Temperatura";
+
+
+            int Celsius = Convert.ToInt32(Console.ReadLine());
+
+
+            // La formula para pasar de Celsius a Fahrehheit es la siguiente : °F = C X 1.8 +32
+            Console.WriteLine("Fahrenheit = {0} °F", Celsius * 1.8 + 32);
+
+
+            // La formula para pasar de Celsius a Kelvin es la siguiente : K= C +273.15
+            Console.WriteLine("Kelvin = {0} K", Celsius +273.15);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
